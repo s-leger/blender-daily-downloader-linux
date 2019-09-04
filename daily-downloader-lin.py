f82673669484def25814e3dcdd0d2eabca520280
s-leger <stephen@3dservices.ch>
2019-09-04 10:44:53 +0200

    Update daily-downloader-lin.py
    Turn into addon with single operator (F3 -> install daily)

--- daily-downloader-lin.py
+++ daily-downloader-lin.py
@@ -27,63 +27,119 @@
 # Author: Stephen Leger (s-leger)
 # ----------------------------------------------------------
 
+bl_info = {
+    'name': 'Install daily',
+    'description': 'Download and setup daily build',
+    'author': 's-leger',
+    'license': 'GPL',
+    'deps': '',
+    'version': (0, 0, 1),
+    'blender': (2, 80, 0),
+    'location': 'search (F3) install daily',
+    'warning': '',
+    'wiki_url': '',
+    'tracker_url': 'https://github.com/s-leger/blender-daily-downloader-linux/issues/',
+    'link': 'https://github.com/s-leger/blender-daily-downloader-linux',
+    'support': 'COMMUNITY',
+    'category': 'Development'
+    }
+
 import requests
 import re
 import os
 import tarfile
 import shutil
-
-# relative sub directory from user home
-setup_subdir = "Softs"
-
-# link to last downloaded version,  /home/user/setup_subdir/blender-daily
-symlink = "blender-daily"
+import bpy
+from bpy.types import Operator
+from bpy.props import StringProperty
 
 
-# Absolute setup path under user home folder
-dl_path = os.path.join(os.path.expanduser("~"), setup_subdir)
+def download_daily(dl_path, symlink):
+    # Absolute setup path under user home folder
 
-if not os.path.exists(dl_path):
-    os.makedirs(dl_path)
+    if not os.path.exists(dl_path):
+        os.makedirs(dl_path)
 
-# retrieve download link for linux builds
-res = requests.get("https://builder.blender.org/download/")
-match = re.search(r'''<li class="os linux"><a href=['"]/download/(.*?)\.tar\.bz2['"].*?(?:</a|/)>''', res.text, re.I)
+    res = requests.get("https://builder.blender.org/download/")
+    match = re.search(r'''<li class="os linux"><a href=['"]/download/(.*?)\.tar\.bz2['"].*?(?:</a|/)>''', res.text, re.I)
 
-if match:
-    filename = match.group(1)
-    url = "https://builder.blender.org/download/%s.tar.bz2" % filename
-    dl_file = os.path.join(dl_path, "%s.tar.bz2" % filename)
-    sym_folder = os.path.join(dl_path, symlink)
-    bz_folder = os.path.join(dl_path, filename)
-    try:
-        shutil.rmtree(bz_folder, ignore_errors=False)
-    except:
-        pass
-    try:
-        os.unlink(dl_file)
-    except:
-        pass
-    res = requests.get(url, stream=True)
-    fsize = int(res.headers.get('content-length'))
-    rsize = 0
-    chunk = 1024 * 8
-    with open(dl_file, "wb") as f:
-        for data in res.iter_content(chunk):
-            f.write(data)
-            rsize += len(data)
-            print("Read %s %%" % (100.0 * rsize / fsize))
-    res.close()
-    if fsize == rsize:
-        tar = tarfile.open(dl_file, "r:bz2")
-        tar.extractall(dl_path)
-        tar.close()
+    if match:
+        filename = match.group(1)
+        url = "https://builder.blender.org/download/%s.tar.bz2" % filename
+        dl_file = os.path.join(dl_path, "%s.tar.bz2" % filename)
+        sym_folder = os.path.join(dl_path, symlink.strip())
+        bz_folder = os.path.join(dl_path, filename)
         try:
-            os.unlink(sym_folder)
+            shutil.rmtree(bz_folder, ignore_errors=False)
         except:
             pass
-        os.symlink(bz_folder, sym_folder)
         try:
             os.unlink(dl_file)
         except:
             pass
+        res = requests.get(url, stream=True)
+        fsize = int(res.headers.get('content-length'))
+        rsize = 0
+        chunk = 1024 * 8
+        with open(dl_file, "wb") as f:
+            for data in res.iter_content(chunk):
+                f.write(data)
+                rsize += len(data)
+                print("Read %s %%" % (100.0 * rsize / fsize))
+        res.close()
+        if fsize == rsize:
+            tar = tarfile.open(dl_file, "r:bz2")
+            tar.extractall(dl_path)
+            tar.close()
+            try:
+                os.unlink(sym_folder)
+            except:
+                pass
+            os.symlink(bz_folder, sym_folder)
+            try:
+                os.unlink(dl_file)
+            except:
+                pass
+            return True
+    return False
+
+
+class SL_OT_install_daily(Operator):
+
+    bl_idname="sl.install_daily"
+    bl_label="Install daily build"
+    bl_description = "Install daily build"
+
+    setup_subdir: StringProperty(
+        name="Setup directory",
+        description="Absolute setup directory (must be writeable)",
+        default=os.path.join(os.path.expanduser("~"), "Soft"),
+        subtype = "DIR_PATH"
+    )
+    symlink : StringProperty(
+        name="symlink",
+        description="symlink to last downloaded version",
+        default="blender-daily")
+
+    def execute(self, context):
+        if self.symlink.strip() != "" and self.setup_subdir.strip() != "":
+            res = download_daily(self.setup_subdir, self.symlink)
+            if res:
+                self.report({"INFO"}, "Setup success")
+            else:
+                self.report({"WARNING"}, "Setup failed")
+        return {'FINISHED'}
+
+    def invoke(self, context, event):
+        return context.window_manager.invoke_props_dialog(self)
+
+
+def register():
+    bpy.utils.register_class(SL_OT_install_daily)
+    # bpy.utils.register_class()
+
+
+def unregister():
+    bpy.utils.unregister_class(SL_OT_install_daily)
+    # bpy.utils.unregister_class()
+
